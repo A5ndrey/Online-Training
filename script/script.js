d0e21a6a3dbda5816e29f01bb975ebeaeb024633
A5ndrey <a5ndrei@mail.ru>
2022-01-28 01:55:00 +0700

    Change files

---
+++ script/script.js
@@ -0,0 +1,50 @@
+function openModalBurger() {
+	document.getElementById("modal").style.top = "0px";	
+}
+function closeModalBurger() {
+	document.getElementById("modal").style.top = "-500px";	
+}
+
+function openModalMarathon() {
+	document.getElementById("modalApplicationMarathon").style.top = "10%";
+	document.getElementById("modalApplicationVebinar2").style.top = "-700px";
+	document.getElementById("modalApplicationVebinar1").style.top = "-700px";
+}
+function closeModalMarathon() {
+	document.getElementById("modalApplicationMarathon").style.top = "-700px";
+}
+
+function openModalVebinar1() {
+	document.getElementById("modalApplicationVebinar1").style.top = "10%";
+	document.getElementById("modalApplicationMarathon").style.top = "-700px";
+	document.getElementById("modalApplicationVebinar2").style.top = "-700px";
+}
+function closeModalVebinar1() {
+	document.getElementById("modalApplicationVebinar1").style.top = "-700px";
+}
+
+function openModalVebinar2() {
+	document.getElementById("modalApplicationVebinar2").style.top = "10%";
+	document.getElementById("modalApplicationVebinar1").style.top = "-700px";
+	document.getElementById("modalApplicationMarathon").style.top = "-700px";
+}
+function closeModalVebinar2() {
+	document.getElementById("modalApplicationVebinar2").style.top = "-700px";
+}
+
+// Smooth Scroll
+
+const links = document.querySelectorAll("a");
+
+for (const link of links) {
+	link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+	e.preventDefault();
+	const href = this.getAttribute("href");
+
+	document.querySelector(href).scrollIntoView({
+		behavior: "smooth"
+	});
+}
